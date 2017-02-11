de79f726d1b1b3c7a33eea5492738ec3228e3ea1
Matt Rice <mattrice12@outlook.com>
2017-02-11 16:11:38 -0600

    Game colony loops around the screen and a new colony regenerates every 20 turns; trying to refactor and test

--- lib/game.go
+++ lib/game.go
@@ -7,20 +7,21 @@ import (
 
 // Game sets up the game board with Rows and Columns that will be manipulated
 type Game struct {
-	Rows int
-	Cols int
+	Rows  int
+	Cols  int
+	Start [][]int
 }
 
 // CreateGame creates the Game and Grid
-func CreateGame(x int, y int) Game {
-	rows := x
-	cols := y
-	game := Game{rows, cols}
+func CreateGame(x int, y int, s [][]int) Game {
+	rows := x + 2
+	cols := y + 2
+	game := Game{rows, cols, s}
 	return game
 }
 
-// BuildGrid builds the grid with all cells dead
-func BuildGrid(g Game) [][]int {
+// BlankGrid builds a grid template with all cells dead
+func BlankGrid(g Game) [][]int {
 	grid := [][]int{}
 	for i := 0; i < g.Cols; i++ {
 		grid = append(grid, []int{})
@@ -31,59 +32,75 @@ func BuildGrid(g Game) [][]int {
 	return grid
 }
 
-// InitialBuild takes a multidimensional array with coordinates for living cells
-func InitialBuild(g *[][]int, arr [][]int) [][]int {
-	for s := range arr {
-		x := arr[s][0]
-		y := arr[s][1]
-		(*g)[x][y] = 1
+// InitialCells takes a multidimensional array with coordinates for living cells
+func InitialCells(grid *[][]int, start [][]int) {
+	for i := range start {
+		x := &start[i][0]
+		y := &start[i][1]
+		(*grid)[*y][*x] = 1
 	}
-	return *g
 }
 
 // RunGame runs the game
-func RunGame(g Game) {
-	grid := BuildGrid(g)
-	start := [][]int{{2, 1}, {2, 2}, {2, 3}, {1, 3}, {0, 2}}
-	InitialBuild(&grid, start)
+func RunGame(game Game) {
+	grid := BlankGrid(game)
+	// source := [][]int{{1, 1}, {1, 2}, {2, 1}, {2, 2}, {2, 3}}
 
 	for i := 0; i < 1000; i++ {
+		Reinitialize(&grid, game, i)
 		PrintGeneration(grid)
-		time.Sleep(time.Second / 4)
-
-		newGrid := BuildGrid(g)
-		for x := 0; x < len(grid); x++ {
-			for y := 0; y < len(grid[x]); y++ {
-				cell := Generation(grid, x, y)
-				newGrid[x][y] = cell
+		newGrid := BlankGrid(game)
+		for y := range grid {
+			for x := range grid[y] {
+				newGrid[y][x] = Generation(grid, y, x)
 			}
 		}
 		grid = newGrid
 	}
 }
 
+// Reinitialize calls the initial build every 20 generations
+func Reinitialize(grid *[][]int, game Game, i int) {
+	if i%20 == 0 {
+		InitialCells(grid, game.Start)
+	}
+}
+
 // Generation determines whether the cell lives or dies based on the amount of living/dead neighbors
-func Generation(g [][]int, x int, y int) int {
-	if x > 0 && x < len(g)-1 && y > 0 && y < len(g[x])-1 {
-		count := 0
-		for i := -1; i <= 1; i++ {
-			for j := -1; j <= 1; j++ {
-				if !(i == 0 && j == 0) {
-					if g[x+i][y+j] == 1 {
-						count++
-					}
+func Generation(grid [][]int, y int, x int) int {
+	y, x = BorderCells(grid, y, x)
+	count := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if !(i == 0 && j == 0) {
+				if grid[y+i][x+j] == 1 {
+					count++
 				}
 			}
 		}
-		c := Rules(count, g, x, y)
-		return c
 	}
-	return 0
+	cell := Rules(count, grid, y, x)
+	return cell
+}
+
+// BorderCells allows Pattern to loop once reaching the bottom of the screen
+func BorderCells(grid [][]int, y int, x int) (int, int) {
+	if y == 0 {
+		y = len(grid) - 2
+	} else if y == len(grid)-1 {
+		y = 1
+	}
+	if x == 0 {
+		x = len(grid[y]) - 2
+	} else if x >= len(grid[y])-1 {
+		x = 1
+	}
+	return y, x
 }
 
 // Rules takes the living neighbor count and determines whether a cell is alive or dead
-func Rules(c int, g [][]int, x int, y int) int {
-	if g[x][y] == 1 {
+func Rules(c int, grid [][]int, y int, x int) int {
+	if grid[y][x] == 1 {
 		if c < 2 || c > 3 {
 			return 0
 		} else if c == 2 || c == 3 {
@@ -97,28 +114,29 @@ func Rules(c int, g [][]int, x int, y int) int {
 }
 
 // PrintGeneration prints out what each generation looks like
-func PrintGeneration(g [][]int) {
+func PrintGeneration(grid [][]int) {
 	clearScreen()
-	for i := 0; i < len(g); i++ {
-		gridString := StringifyRow((g)[i])
-		for _, a := range gridString {
-			fmt.Print(a)
+	for i := 0; i < len(grid)-2; i++ {
+		gridString := StringifyRow((grid)[i])
+		for i := 0; i < len(gridString)-2; i++ {
+			fmt.Print(gridString[i])
 		}
 		fmt.Println()
 	}
+	time.Sleep(time.Second / 10)
 }
 
 // StringifyRow turns integer into string
 func StringifyRow(row []int) []string {
-	arr := []string{}
-	for _, b := range row {
-		if b == 0 {
-			arr = append(arr, " ")
+	strArr := []string{}
+	for _, cell := range row {
+		if cell == 0 {
+			strArr = append(strArr, " ")
 		} else {
-			arr = append(arr, "@")
+			strArr = append(strArr, "*")
 		}
 	}
-	return arr
+	return strArr
 }
 
 func clearScreen() {
--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import "github.com/mattrice12/game-of-life/lib"
 
 func main() {
-	game := lib.CreateGame(40, 20)
+	width := 30
+	height := 15
+	start := [][]int{{3, 2}, {3, 3}, {3, 4}, {2, 4}, {1, 3}}
 
+	game := lib.CreateGame(width, height, start)
 	lib.RunGame(game)
 }
